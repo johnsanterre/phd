bf6f782541eeba205d293e1365c55acceb799833
John Santerre <john@johnsanterre.com>
2016-12-20 19:27:57 -0500

    fixed

--- code/file_utils.py
+++ code/file_utils.py
@@ -1,6 +1,6 @@
 import os.path
 
-def make_file_names(name, directory='./'):
+def make_file_names(name, directory='../../'):
   ret = []
   for i in range(10,21):
     file_name = directory + name + str(i) + '.npy'
